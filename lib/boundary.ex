ddc97bf39756d624ce5a7bc5b83fa7bca0f683b2
Muhammad Ilham Hidayat <m.ilham.hidayat.95@gmail.com>
2024-04-01 17:22:25 +0700

    feat: calculator with otp

--- lib/boundary.ex
+++ lib/boundary.ex
@@ -2,7 +2,7 @@ defmodule Calculator.Boundary do
   alias Calculator.Core
 
   def start(initial_state) do
-    spawn(fn -> run(initial_state) end)
+    spawn(fn -> run(initial_state) end) # <-- init
   end
 
   def run(state) do
@@ -13,11 +13,15 @@ defmodule Calculator.Boundary do
 
   def listen(state) do
     receive do
+      # handle cast
       {:add, number} -> Core.add(state, number)
+      # handle cast
       {:substract, number} -> Core.substract(state, number)
+      # handle cast
       {:multiply, number} -> Core.multiply(state, number)
+      # handle cast
       {:divide, number} -> Core.divide(state, number)
-      :clear -> 0
+      # handle call
       {:state, pid} ->
         send(pid, {:state, state})
         state
